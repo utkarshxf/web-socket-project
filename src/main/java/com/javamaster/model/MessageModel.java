a6ae677c93687b89acad051df084aeec085c11f7
utkarsh <utkarshsinghrajput16@gmail.com>
2023-08-10 01:48:44 +0530

    .

---
+++ src/main/java/com/javamaster/model/MessageModel.java
@@ -0,0 +1,31 @@
+package com.javamaster.model;
+
+public class MessageModel {
+
+    private String message;
+    private String fromLogin;
+
+    @Override
+    public String toString() {
+        return "MessageModel{" +
+                "message='" + message + '\'' +
+                ", fromLogin='" + fromLogin + '\'' +
+                '}';
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getFromLogin() {
+        return fromLogin;
+    }
+
+    public void setFromLogin(String fromLogin) {
+        this.fromLogin = fromLogin;
+    }
+}
